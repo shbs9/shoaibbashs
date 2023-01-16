123e6642b2807271b85d219bb8633c480162ece9
Shoaib khan khattak <39294639+shbs9@users.noreply.github.com>
2023-01-16 15:08:55 +0500

    removed extra work

--- cyberpanelversion.sh
+++ cyberpanelversion.sh
@@ -17,23 +17,5 @@ chmod +x cyberpanel_upgrade.sh
 ./cyberpanel_upgrade.sh
 
 
-# Function to give out the script
-give_out_script() {
-  echo "Here is the script:"
-  cat "$0"
-}
-
-# Check if the user wants the script
-echo "Do you want to see the script? (y/n)"
-read -r answer
-
-if [[ "$answer" == "y" ]]; then
-  give_out_script
-  exit 0
-fi
-
-# Perform some processes
-echo "Performing some processes..."
-sleep 2
-echo "Processes completed!"
+#ended here
 
